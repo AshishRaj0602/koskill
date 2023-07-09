ac6fc371f7e3474edbeaa73795b5732dce76bc6b
Ashish Rajpoot <115613437+AshishRaj0602@users.noreply.github.com>
2023-07-10 00:09:35 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import SignUp from "./pages/SignUp.jsx";
 import Home from "./pages/Home.jsx";
 import { register } from './store/userSlice';
 function App() {
   const dispatch = useDispatch();
-  
-  let user={};
+  let [user,setUser]=useState(null);
   useEffect(() => {
       // eslint-disable-next-line react-hooks/exhaustive-deps
       user = JSON.parse(localStorage.getItem("User"));
@@ -18,7 +17,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-          <Route path="/" element={user?<Home/>:<Navigate to="/login"/>} />
+          <Route path="/" element={user?<Home/>:<Login/>} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<Navigate to="/" />} />
